d388306d036e5c2559f7fe10d56882233ef5322e
andreas <andreas.schroden@gmail.com>
2018-03-30 03:20:30 +0200

    Improved cover-handling

--- src/library/M4bTool/Command/MergeCommand.php
+++ src/library/M4bTool/Command/MergeCommand.php
@@ -74,9 +74,6 @@ class MergeCommand extends AbstractConversionCommand
 
         $this->loadInputFiles();
 
-
-
-
         $this->loadInputMetadataFromFirstFile();
         $this->convertInputFiles();
 
@@ -155,10 +152,30 @@ class MergeCommand extends AbstractConversionCommand
         $this->setOptionIfUndefined("year", $metaData->getProperty("date"));
         $this->setOptionIfUndefined("genre", $metaData->getProperty("genre"));
         $this->setOptionIfUndefined("writer", $metaData->getProperty("writer"));
-        if ($this->argInputFile->isDir()) {
+        if ($this->argInputFile->isDir() && !$this->input->getOption("skip-cover")) {
             $autoCoverFile = new SplFileInfo($this->argInputFile . DIRECTORY_SEPARATOR . "cover.jpg");
             if ($autoCoverFile->isFile()) {
                 $this->setOptionIfUndefined("cover", $autoCoverFile);
+            } else {
+                $autoCoverFile = null;
+                $iterator = new \DirectoryIterator($this->argInputFile);
+                foreach ($iterator as $potentialCoverFile) {
+                    if ($potentialCoverFile->isDot() || $potentialCoverFile->isDir()) {
+                        continue;
+                    }
+
+                    $lowerExt = strtolower(ltrim($potentialCoverFile->getExtension(), "."));
+                    if ($lowerExt === "jpg" || $lowerExt === "jpeg" || $lowerExt === "png") {
+                        $autoCoverFile = clone $potentialCoverFile->getFileInfo();
+                        break;
+
+                    }
+
+                }
+
+                if ($autoCoverFile && $autoCoverFile->isFile()) {
+                    $this->setOptionIfUndefined("cover", $autoCoverFile);
+                }
             }
         }
 
@@ -182,13 +199,13 @@ class MergeCommand extends AbstractConversionCommand
             throw new Exception("Could not create temp directory " . $dir);
         }
 
-        if($this->optAdjustBitrateForIpod)  {
+        if ($this->optAdjustBitrateForIpod) {
             $this->output->writeln("ipod auto adjust active, getting track durations");
             $this->totalDuration = new TimeUnit();
             foreach ($this->filesToConvert as $index => $file) {
                 $duration = $this->readDuration($file);
                 if (!$duration) {
-                    throw new Exception("could not get duration for file ".$file." - needed for ". static::OPTION_ADJUST_FOR_IPOD);
+                    throw new Exception("could not get duration for file " . $file . " - needed for " . static::OPTION_ADJUST_FOR_IPOD);
                 }
                 $this->totalDuration->add($duration->milliseconds());
             }
@@ -205,36 +222,35 @@ class MergeCommand extends AbstractConversionCommand
 
             $durationSeconds = $this->totalDuration->milliseconds() / 1000;
             $maxSamplingRate = 2147483647 / $durationSeconds;
-            $this->output->writeln("total duration: ".$this->totalDuration->format("%H:%I:%S.%V")." (".$durationSeconds."s)");
-            $this->output->writeln("max possible sampling rate: ".$maxSamplingRate."Hz");
-            $this->output->writeln("desired sampling rate: ".$this->optAudioSampleRate."Hz");
+            $this->output->writeln("total duration: " . $this->totalDuration->format("%H:%I:%S.%V") . " (" . $durationSeconds . "s)");
+            $this->output->writeln("max possible sampling rate: " . $maxSamplingRate . "Hz");
+            $this->output->writeln("desired sampling rate: " . $this->optAudioSampleRate . "Hz");
 
-            if($this->samplingRateToInt() > $maxSamplingRate)  {
-                $this->output->writeln("desired sampling rate ".$this->optAudioSampleRate." is greater than max sampling rate ".$maxSamplingRate."Hz, trying to adjust");
+            if ($this->samplingRateToInt() > $maxSamplingRate) {
+                $this->output->writeln("desired sampling rate " . $this->optAudioSampleRate . " is greater than max sampling rate " . $maxSamplingRate . "Hz, trying to adjust");
                 $resultSamplingRate = 0;
                 $resultBitrate = "";
-                foreach($samplingRateToBitrateMapping as $samplingRate => $bitrate)  {
-                    if($samplingRate <= $maxSamplingRate)  {
+                foreach ($samplingRateToBitrateMapping as $samplingRate => $bitrate) {
+                    if ($samplingRate <= $maxSamplingRate) {
                         $resultSamplingRate = $samplingRate;
                         $resultBitrate = $bitrate;
-                    } else   {
+                    } else {
                         break;
                     }
                 }
 
-                if($resultSamplingRate === 0)   {
-                    throw new Exception("Could not find an according setting for ".static::OPTION_AUDIO_BIT_RATE." / ".static::OPTION_AUDIO_SAMPLE_RATE. " for option ".static::OPTION_ADJUST_FOR_IPOD);
+                if ($resultSamplingRate === 0) {
+                    throw new Exception("Could not find an according setting for " . static::OPTION_AUDIO_BIT_RATE . " / " . static::OPTION_AUDIO_SAMPLE_RATE . " for option " . static::OPTION_ADJUST_FOR_IPOD);
                 }
 
                 $this->optAudioSampleRate = $resultSamplingRate;
                 $this->optAudioBitRate = $resultBitrate;
-                $this->output->writeln("adjusted to ".$resultBitrate."/".$resultSamplingRate);
-            } else   {
+                $this->output->writeln("adjusted to " . $resultBitrate . "/" . $resultSamplingRate);
+            } else {
                 $this->output->writeln("desired sampling rate is ok, nothing to change");
             }
 
         }
-
 
 
         foreach ($this->filesToConvert as $index => $file) {
@@ -279,9 +295,14 @@ class MergeCommand extends AbstractConversionCommand
             */
 
             // workaround incompatible files (only for windows systems)
-            if ($this->isWindows()) {
-                $command[] = "-vf";
-                $command[] = "scale=800:800";
+//            if ($this->isWindows()) {
+//                $command[] = "-vf";
+//                $command[] = "scale=800:800";
+//            }
+
+            // workaround for coversize not divided by 2
+            if (!$this->input->getOption("skip-cover")) {
+                $this->appendParameterToCommand($command, "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2");
             }
 
             // Relocating moov atom to the beginning of the file can facilitate playback before the file is completely downloaded by the client.
@@ -294,6 +315,8 @@ class MergeCommand extends AbstractConversionCommand
             $this->appendParameterToCommand($command, "-ac", $this->optAudioChannels);
             $this->appendParameterToCommand($command, "-acodec", $this->optAudioCodec);
             $this->appendParameterToCommand($command, "-f", $this->optAudioFormat);
+
+
             $command[] = $outputFile;
 
             $this->ffmpeg($command, "converting " . $file . " to " . $outputFile . "");
@@ -321,7 +344,7 @@ class MergeCommand extends AbstractConversionCommand
             $metaData = $this->readFileMetaData($file);
             $duration = $this->readDuration($file);
             if (!$duration) {
-                throw new Exception("could not get duration for file ".$file);
+                throw new Exception("could not get duration for file " . $file);
             }
 
             $start = $this->totalDuration->milliseconds();
@@ -361,6 +384,22 @@ class MergeCommand extends AbstractConversionCommand
         ];
         $this->chapters = $chapterMarker->normalizeChapters($this->chapters, $options);
 
+    }
+
+    private function addTrackMarkers()
+    {
+        if (!$this->input->getOption(static::OPTION_MARK_TRACKS)) {
+            return;
+        }
+
+
+        foreach ($this->trackMarkerSilences as $index => $silence) {
+            $key = $silence->getStart()->milliseconds();
+            if (!isset($this->chapters[$key])) {
+                $this->chapters[$key] = new Chapter(clone $silence->getStart(), clone $silence->getLength(), "Track marker " . ($index + 1));
+            }
+        }
+        ksort($this->chapters);
     }
 
     private function mergeFiles()
@@ -405,7 +444,6 @@ class MergeCommand extends AbstractConversionCommand
 
     }
 
-
     private function importChapters()
     {
 
@@ -438,21 +476,5 @@ class MergeCommand extends AbstractConversionCommand
     {
         $tag = $this->inputOptionsToTag();
         $this->tagFile($this->outputFile, $tag);
-    }
-
-    private function addTrackMarkers()
-    {
-        if(!$this->input->getOption(static::OPTION_MARK_TRACKS)) {
-            return;
-        }
-
-
-        foreach($this->trackMarkerSilences as $index => $silence) {
-            $key = $silence->getStart()->milliseconds();
-            if(!isset($this->chapters[$key])) {
-                $this->chapters[$key] = new Chapter(clone $silence->getStart(), clone $silence->getLength(), "Track marker ".($index+1));
-            }
-        }
-        ksort($this->chapters);
     }
 }
