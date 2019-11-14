d389401fe6a1f33d701108a7c35171773d685e4e
andreas <andreas.schroden@gmail.com>
2019-11-14 13:41:49 +0100

    improved intro-outro-chapters improver

--- src/library/M4bTool/Audio/Tag/IntroOutroChapters.php
+++ src/library/M4bTool/Audio/Tag/IntroOutroChapters.php
@@ -15,15 +15,17 @@ class IntroOutroChapters implements TagImproverInterface
         if (count($tag->chapters) === 0) {
             return $tag;
         }
-        $firstChapter = $tag->chapters[0];
-        if ($firstChapter->getName() !== Chapter::DEFAULT_INTRO_NAME) {
+        reset($tag->chapters);
+        $firstChapter = current($tag->chapters);
+        if ($firstChapter && $firstChapter->getName() !== Chapter::DEFAULT_INTRO_NAME) {
             $introChapter = new Chapter(new TimeUnit(0), new TimeUnit(10, TimeUnit::SECOND), Chapter::DEFAULT_INTRO_NAME);
             array_unshift($tag->chapters, $introChapter);
             $firstChapter->setStart(clone $introChapter->getEnd());
         }
 
-        $lastChapter = $tag->chapters[count($tag->chapters) - 1];
-        if ($lastChapter->getName() !== Chapter::DEFAULT_OUTRO_NAME) {
+
+        $lastChapter = end($tag->chapters);
+        if ($lastChapter && $lastChapter->getName() !== Chapter::DEFAULT_OUTRO_NAME) {
             $outroLength = new TimeUnit(10, TimeUnit::SECOND);
             $outroStart = new TimeUnit($lastChapter->getEnd()->milliseconds() - $outroLength->milliseconds());
             $outroChapter = new Chapter($outroStart, $outroLength, Chapter::DEFAULT_OUTRO_NAME);
